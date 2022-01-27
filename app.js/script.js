9d7129a9457c75657eb552d6f140cc6363cfb6a8
dee chao <chaoserady@gmail.com>
2022-01-27 12:48:48 -0600

    first commit

---
+++ app.js/script.js
@@ -0,0 +1,44 @@
+// USE STRICT MODE
+
+"use strict";
+
+// GRABBING DOM ELEMENTS
+
+console.log("hello");
+
+const header = document.querySelector(".header");
+const burgerBox = document.querySelector(".burger-box");
+const nav = document.querySelector(".nav");
+const lines = document.querySelectorAll(".lines");
+const topLine = document.querySelector(".top");
+const middle = document.querySelector(".middle");
+const bottomLine = document.querySelector(".bottom");
+
+burgerBox.addEventListener("click", () => {
+  nav.classList.toggle("navShow");
+  topLine.classList.toggle("topChange");
+  middle.classList.toggle("middleChange");
+  bottomLine.classList.toggle("bottomChange");
+});
+
+function linesColorStay() {
+  lines.forEach((l) => {
+    l.style.backgroundColor = "#fff";
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 450) {
+    lines.forEach((l) => {
+      l.classList.add("lineChange");
+    });
+  } else {
+    lines.forEach((l) => {
+      l.classList.remove("lineChange");
+    });
+  }
+});
+
+// middle.addEventListener("click", (e) => {
+//   console.log(e);
+// });
