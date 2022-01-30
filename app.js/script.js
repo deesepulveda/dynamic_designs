46b0b9764043c7f8205af32dcd54e10f4670003e
dee chao <chaoserady@gmail.com>
2022-01-30 14:04:37 -0600

    updated media queries for top portion of jumbo

--- app.js/script.js
+++ app.js/script.js
@@ -4,15 +4,17 @@
 
 // GRABBING DOM ELEMENTS
 
-console.log("hello");
-
 const header = document.querySelector(".header");
 const burgerBox = document.querySelector(".burger-box");
 const nav = document.querySelector(".nav");
+const diamondEnds = document.querySelector(".diamond-ends");
+const links = document.querySelectorAll(".links");
 const lines = document.querySelectorAll(".lines");
 const topLine = document.querySelector(".top");
 const middle = document.querySelector(".middle");
 const bottomLine = document.querySelector(".bottom");
+
+// SHOW NAV ON CLICK
 
 burgerBox.addEventListener("click", () => {
   nav.classList.toggle("navShow");
@@ -20,6 +22,8 @@ burgerBox.addEventListener("click", () => {
   middle.classList.toggle("middleChange");
   bottomLine.classList.toggle("bottomChange");
 });
+
+// CHANGE BURGER LINE COLOR AFTER SCROLL
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 450) {
@@ -33,6 +37,22 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// middle.addEventListener("click", (e) => {
-//   console.log(e);
-// });
+// HEADER BG COLOR APPEAR AFTER SCROLL IN LANDSCAPE MODE
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.style.backgroundColor = "#fff";
+    header.style.boxShadow = "1px 1px 5px rgba(5,5,5,.3";
+    diamondEnds.style.display = "none";
+    links.forEach((li) => {
+      li.style.color = "#050505";
+    });
+  } else {
+    header.style.backgroundColor = "transparent";
+    header.style.boxShadow = "none";
+    diamondEnds.style.display = "block";
+    links.forEach((li) => {
+      li.style.color = "#fff";
+    });
+  }
+});
